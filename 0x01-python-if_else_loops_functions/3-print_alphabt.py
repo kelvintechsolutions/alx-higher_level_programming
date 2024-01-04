b8a5a80e9122e3a7e1151664af54805f50f22eb7
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-03 22:03:20 -0800

    more task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'e' or chr(i) == 'q':
-        continue
-    else:
-        print("{:s}".format(chr(i)), end="")
+for alphabet_letter in range(ord('a'), ord('z') + 1):
+    if chr(alphabet_letter) != 'e' and chr(alphabet_letter) != 'q':
+        print('{:c}'.format(alphabet_letter), end='')
