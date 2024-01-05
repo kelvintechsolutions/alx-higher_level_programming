381a6fc40a37122067f7ce3a316da755997de8bf
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-05 02:07:01 -0800

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+def summation_arguments(argv):
+    y = len(argv) - 1
+    if y == 0:
+        print("{:d}".format(y))
+        return
+    else:
+        index = 1
+        summation = 0
+        while index <= y:
+            summation += int(argv[index])
+            index += 1
+        print("{}".format(summation))
+
+
 if __name__ == "__main__":
-
     import sys
-
-    value = 0
-    for i in range(len(sys.argv) - 1):
-        value = value + int(sys.argv[i + 1])
-    print("{}".format(value))
+    summation_arguments(sys.argv)
