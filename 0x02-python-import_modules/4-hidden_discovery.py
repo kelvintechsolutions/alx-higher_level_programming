b89ffe82a58fd16e5435aa0120b91af8cd1ae27f
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-05 02:11:29 -0800

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    for name in sorted(dir(hidden_4)):
-        if name[:2] != '__':
-            print("{}".format(name))
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
