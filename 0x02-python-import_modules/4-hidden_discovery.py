c586243313a87d7508cecd3b08a09577630ababf
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-05 02:08:42 -0800

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     import hidden_4
-    built = dir(hidden_4)
-    for name in built:
-        if name[:2] != "__":
-            print(name)
+    for name in sorted(dir(hidden_4)):
+        if name[:2] != '__':
+            print("{}".format(name))
