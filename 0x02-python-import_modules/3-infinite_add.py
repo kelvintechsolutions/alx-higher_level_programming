3e03f7a2b5018190b96488ec59ad877018b645c1
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-10 11:49:34 -0800

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-def summation_arguments(argv):
-    y = len(argv) - 1
-    if y == 0:
-        print("{:d}".format(y))
-        return
-    else:
-        index = 1
-        summation = 0
-        while index <= y:
-            summation += int(argv[index])
-            index += 1
-        print("{}".format(summation))
-
 
 if __name__ == "__main__":
+    """Print the addition of all arguments."""
     import sys
-    summation_arguments(sys.argv)
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
