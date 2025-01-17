f44c373f099b3274b7f319be307a1239b0c622e2
kelvintechsolutions <kevohmutugi51@gmaIL.com>
2025-01-17 03:43:25 -0800

    updates

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-"""Prints the GitHub id of a user."""
+"""lists the 10 most recent commits on a given GitHub repository.
+"""
 import sys
 import requests
-import base64
 
-if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        url = "https://api.github.com/user"
-        username = sys.argv[1]
-        password = sys.argv[2]
 
-        # Encode the username and password (or personal access token) in Base64
-        credentials = '{}:{}'.format(username, password)
-        encoded_credentials = base64.b64encode(
-            credentials.encode('utf-8')).decode('utf-8')
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-        req_headers = {
-            'Accept': 'application/vnd.github.v3+json',
-            'Authorization': 'Basic {}'.format(encoded_credentials),
-        }
-        response = requests.get(url, headers=req_headers)
-
-        if response.status_code == 200:
-            user = response.json()
-
-            if user.get('login') == username:
-                print(user.get('id'))
-            else:
-                print('None')
-        else:
-            print('None')
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
